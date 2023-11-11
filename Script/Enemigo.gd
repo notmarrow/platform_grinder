c7aa347bdcae11e68410cdf955812b5b9fecc788
“Fabio” <eragonvaldespaz@gmail.com>
2023-11-10 21:16:31 -0600

    enemigo_A3

--- Script/Enemigo.gd
+++ Script/Enemigo.gd
@@ -1,20 +1,26 @@
 extends CharacterBody2D
 
 var SPEED = 200
-const RayCast_suelo_POSITION_x = 44
-const RayCast_pared_TARGET_POSITION_X = 45
+var steps_to_move_right = 50  # Ajusta estos valores según tus necesidades
+var steps_to_move_left = 50
+var current_steps = 0
+var moving_right = true
 
 func _ready():
 	velocity.x = SPEED
-	$RayCast_suelo.position.x = RayCast_suelo_POSITION_x
-	$RayCast_pared.target_position.x = RayCast_pared_TARGET_POSITION_X
 
 func _physics_process(delta):
-	
-	if not $RayCast_suelo.is_colliding() or $RayCast_pared.is_colliding():
-		velocity.x *= -1
-		$RayCast_suelo.position.x *= -1
-		$RayCast_pared.target_position.x *= -1
+	if moving_right:
+		velocity.x = SPEED
+	else:
+		velocity.x = -SPEED
 
 	move_and_slide()
 
+	# Actualizar el número de pasos
+	current_steps += 1
+
+	# Cambiar de dirección cuando alcance el número de pasos máximo
+	if current_steps >= steps_to_move_right + steps_to_move_left:
+		current_steps = 0
+		moving_right = not moving_right
