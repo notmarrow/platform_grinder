eb48733452dcd28886453bd27511f3b3b76222e3
“Fabio” <eragonvaldespaz@gmail.com>
2023-11-10 21:35:13 -0600

    enemigo_A4

--- Script/Enemigo.gd
+++ Script/Enemigo.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
 var SPEED = 200
-var steps_to_move_right = 50  # Ajusta estos valores según tus necesidades
-var steps_to_move_left = 50
+var steps_to_move_right = 250 # Ajusta este valor según tus necesidades
 var current_steps = 0
-var moving_right = true
 
 func _ready():
 	velocity.x = SPEED
 
 func _physics_process(delta):
-	if moving_right:
+	if current_steps < steps_to_move_right:
+		# Mover hacia la derecha
 		velocity.x = SPEED
 	else:
+		# Giro y mover hacia la izquierda
 		velocity.x = -SPEED
 
 	move_and_slide()
@@ -20,7 +20,6 @@ func _physics_process(delta):
 	# Actualizar el número de pasos
 	current_steps += 1
 
-	# Cambiar de dirección cuando alcance el número de pasos máximo
-	if current_steps >= steps_to_move_right + steps_to_move_left:
+	# Reiniciar cuando alcanza el número de pasos máximo
+	if current_steps >= steps_to_move_right * 2:
 		current_steps = 0
-		moving_right = not moving_right
