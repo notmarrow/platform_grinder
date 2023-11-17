9bc712fed18caaa363d56da43ecd0a5f91d7b21e
worram <javierrdzmtz2002@gmail.com>
2023-11-16 22:46:35 -0700

    movimiento terminado

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,39 @@
 extends CharacterBody2D
 
+#movement variables
+@export var SPEED : float = 300.0
+@export var JUMP_VELOCITY : float = -40.0
+@export var DASH_SPEED : float = 300.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var canDash = true
+var impulse = 0.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var input_delay = 0.5
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		canDash = true
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("space") and is_on_floor():
+		impulse += JUMP_VELOCITY
+		
+	if Input.is_action_just_released("space") and is_on_floor():
+		if impulse < -900:
+			impulse = -900
+		velocity.y = impulse
+		impulse = 0.0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("a", "d")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_just_pressed("click") and canDash:
+		canDash = false
+		position.x += direction * DASH_SPEED
+
 	move_and_slide()
+
